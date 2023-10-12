a191707d5454f49b4ec3899ce3bd900c55701b74
franenlanube <ciscosolana@gmail.com>
2023-10-12 15:53:37 +0200

    Add validations. Get anonymous when nickname is null, cifrate password

--- DiceGame/app/Http/Controllers/UserController.php
+++ DiceGame/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
 
 class UserController extends Controller
 {
@@ -14,23 +15,16 @@ class UserController extends Controller
     {
         //
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
     // TODO add parameters required at the block quote in bodyParam sections
 
     /**
-     * Store a new user.
+     * REGISTER AND STORE A NEW USER.
      *
      * @group Users
      * @bodyParam nickname. The nickname of the user. Example: userNickname
      * @bodyParam email. The email of the user. Example: user@mail.com
      * @bodyParam password. The password of the user. Example: secret_password
+     * @bodyParam password_confirmation. It must match with password
      *
      * @response 201 {
      *     "message": "User created successfully",
@@ -41,37 +35,36 @@ class UserController extends Controller
      *     }
      * }
      * @response 400 {
-     *     "message": "This email is already registered",
-     *     "message": "This nickname is already taken"
+     *     "message": "Different Laravel validation messages from lang/en/validation.php",
      * }
      */
-    public function store(Request $request)
+    public function register(Request $request)
     {
-        //TODO Add validations
-        // Check if user is already created
-        $existingUser = User::where('email', $request->email)->first();
+        // Define validation rules
+        $rules = [
+            'nickname' => 'nullable|unique:users',
+            'email' => 'required|email|unique:users',
+            'password' => [
+                'required', 'confirmed', Password::min(8)
+                //->letters()
+                //->mixedCase()
+                //->numbers()
+                //->symbols()
+                // TODO uncomment this validations rules if it's wished
+            ],
+        ];
+        // Validate request inputs
+        $validator = validator($request->all(), $rules);
 
-        //TODO Pensar la lógica para el usuario anónimo
-        //$name = $request->filled('nickname') ? $request->name : 'anonymous';
-
-        // Get user nickname
-        $existingUserNickname = User::where('nickname', $request->nickname)->first();
-
-        if ($existingUser /* != null */) { // FIXME clean this
-
-            return response()->json(['message' => 'This email is already registered'], 400);
-        } else if ($existingUserNickname /* != null */) { // FIXME clean this
-
-            return response()->json(['message' => 'This nickname is already taken'], 400);
+        if ($validator->fails()) {
+            // Get a 422 response with validation errors
+            return response()->json(['message' => 'Validation failed', 'errors' => $validator->errors()], 422);
         }
-        if ($user = User::create([
-            'nickname' => $request->nickname,
-            'email' => $request->email,
-            'password' => $request->password, //TODO add encryptation
-            //TODO add pass double confirmation
-        ])) // TODO asign role
+        if ($user = User::create($request->input())) // TODO asign role
         {
             // If creations works
+            // Asign "Anonymous" if nickname is null
+            $user->nickname = $user->nickname ?? 'Anonymous';
             return response()->json(['message' => 'User created successfully', 'data' => $user], 201);
         };
 
@@ -82,10 +75,73 @@ class UserController extends Controller
 
         // If creations fails
         return response()->json(['error' => 'User has not been created'], 400);
+        // TODO Add the rest of atributes to edit
+    }
+    /**
+     * UPDATE A SPECIFIC USER.
+     *
+     * @group Users
+     * @urlParam id required The ID of the user.
+     * @bodyParam nickname. The nickname of the user. Example: userNickname
+     *
+     * @response 200 {
+     *     "message": "User successfully updated",
+     *     "data": {
+     *         "id": 1,
+     *         "nickname": "userNickname",
+     *         "email": "user@mail.com"
+     *     }
+     * }
+     * * @response 404 {
+     *     "error": "User not found"
+     * }
+     */
+    public function update(Request $request, string $id)
+    {
+        // Define validation rules
+        $rules = [
+            'nickname' => 'nullable|unique:users,nickname,'
+                // TODO uncomment this validations if update this fields is wanted
+                /*,'email' => 'required|email|unique:users',
+                'password' => [
+                current_password:api',
+                required', 'confirmed', Password::min(8)
+                    ->letters()
+                    ->mixedCase()
+                    ->numbers()
+                    ->symbols() ] */
+                . $id, // To ignore user field values in validation
+        ];
+
+        // Validate request inputs
+        $validator = validator($request->all(), $rules);
+
+        if ($validator->fails()) {
+            // Get a 422 response with validation errors
+            return response()->json(['message' => 'Validation failed', 'errors' => $validator->errors()], 422);
+        }
+
+        //TODO Pensar la lógica para el usuario anónimo
+        //$name = $request->filled('nickname') ? $request->name : 'anonymous';
+        // Get user nickname
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        $user->nickname = $request->input('nickname');
+
+        $user->update();
+        // If creations works
+        // Asign "Anonymous" if nickname is null
+        $user->nickname = $user->nickname ?? 'Anonymous';
+
+        return response()->json(['message' => 'User successfully updated', 'data' => $user], 200);
     }
 
     /**
-     * Get a specific user by ID.
+     * GET A SPECIFIC USER BY ID.
      *
      * @group Users
      * @urlParam id required The ID of the user.
@@ -109,57 +165,13 @@ class UserController extends Controller
             return response()->json(['error' => 'User not found'], 404);
         }
         // User is found
+        // Asign "Anonymous" if nickname is null
+        $user->nickname = $user->nickname ?? 'Anonymous';
         return response()->json(['message' => 'User found', 'data' => $user], 200);
     }
 
     /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-    // TODO Add the rest of atributes to edit
-    /**
-     * Update a specific user.
-     *
-     * @group Users
-     * @urlParam id required The ID of the user.
-     * @bodyParam nickname. The nickname of the user. Example: userNickname
-     *
-     * @response 200 {
-     *     "message": "User successfully updated",
-     *     "data": {
-     *         "id": 1,
-     *         "nickname": "userNickname",
-     *         "email": "user@mail.com"
-     *     }
-     * }
-     * * @response 404 {
-     *     "error": "User not found"
-     * }
-     */
-    public function update(Request $request, string $id)
-    {
-
-        $user = User::find($id);
-
-        if (!$user) {
-            return response()->json(['error' => 'User not found'], 404);
-        }
-
-        // TODO Add validations
-        if ($request->has('nickname')) {
-            User::find($id)->update([
-                'nickname' =>  $request->nickname
-            ]);
-        }
-
-        return response()->json(['message' => 'User successfully updated', 'data' => $user], 200);
-    }
-
-    /**
-     * Remove a specific user by ID.
+     * REMOVE A SPECIFIC USER BY ID.
      *
      * @group Users
      * @urlParam id required The ID of the user.
@@ -173,7 +185,7 @@ class UserController extends Controller
      * }
      */    public function destroy(string $id)
     {
-        //This method is not asked at the project
+        //TODO This method is not asked at the project
         $user = User::find($id);
         if (!$user) {
             return response()->json(['error' => 'User not found'], 404);

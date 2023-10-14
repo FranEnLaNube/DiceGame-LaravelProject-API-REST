94530b08a34e55c953e9a2df076dbf109efe1b1b
franenlanube <ciscosolana@gmail.com>
2023-10-14 14:20:27 +0200

    Use DatabaseTransactions & Passport acting as user

--- DiceGame/tests/Feature/UserControlerUpdateTest.php
+++ DiceGame/tests/Feature/UserControlerUpdateTest.php
@@ -3,28 +3,28 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Laravel\Passport\Passport;
 use Tests\TestCase;
 
 class UserControlerUpdateTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseTransactions;
 
     public function test_user_can_update_with_valid_data()
     {
         // Create a new user at testing database
         $user = User::factory()->create();
         // Authenticate $user if route is protected
-        // TODO uncomment this when protect routes
-        //$this->actingAs($user);
+        Passport::actingAs($user);
         $newUserData = [
             'nickname' => 'NewNickname',
         ];
         // Make a PUT request to update user route with new data
         $response = $this->put(route('player.update', ['id' => $user->id]), $newUserData);
-        // TODO Check if is correct a 200 or a 201 response
-        $response->assertStatus(200); // Check if gets a HTTP 201 response (registered succesfully).
+        // Check if gets a HTTP 200 response (registered succesfully).
+        $response->assertStatus(200);
         // Check if the user is updated in database
         $this->assertDatabaseHas('users', ['nickname' => 'NewNickname']);
     }
@@ -34,12 +34,9 @@ class UserControlerUpdateTest extends TestCase
         // Create two new users at testing database
         $user1 = User::factory()->create();
         $user2 = User::factory()->create();
-        // Authenticate $user if route is protected
-        // TODO uncomment this when protect routes
-        //$this->actingAs($user1);
-        //$this->actingAs($user2);
+        // Authenticate $user
+        Passport::actingAs($user2);
 
-        // Intenta actualizar el usuario 2 con el 'nickname' del usuario 1
         // Try to update User2 with User1's nickname
         $response = $this->put(route('player.update', ['id' => $user2->id]), ['nickname' => $user1->nickname]);
 
@@ -53,11 +50,12 @@ class UserControlerUpdateTest extends TestCase
     {
         // Create a new user at testing database
         $user = User::factory()->create();
+        Passport::actingAs($user);
         $newUserData = [
             'nickname' => 'NewNickname',
         ];
         // Make a PUT request to update user route with new data
-        $response = $this->put(route('player.update', ['id' => ($user->id + 1)]), $newUserData);
+        $response = $this->put(route('player.update', ['id' => ($user->id * -1)]), $newUserData);
         $response->assertStatus(404); // Check if gets a HTTP 404 response (user not found).
     }
 }

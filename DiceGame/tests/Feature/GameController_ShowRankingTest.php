b21338ece1f049a5ea684573fab5e6b53bed2b41
franenlanube <ciscosolana@gmail.com>
2023-10-19 11:34:32 +0200

    New tests for showLoser, showWinner and showRanking methods

---
+++ DiceGame/tests/Feature/GameController_ShowRankingTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use App\Models\User;
+use Laravel\Passport\Passport;
+
+class GameController_ShowRankingTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function test_admin_can_get_ranking()
+    {
+        // Create a "admin" user and give them the "admin" permission
+        Passport::actingAs(User::factory()->create(), ['admin']);
+
+        // Fetch the best player
+        $response = $this->get(route('admin.showWinner'));
+
+        // Check for a 200 status code
+        $response->assertStatus(200);
+
+        // Check JSON response structure
+        $response->assertJsonStructure([
+            'message',
+        ]);
+    }
+
+    public function test_admin_cannot_see_ranking()
+    {
+        // Create a "player" user and give them the "player" permission
+        Passport::actingAs(User::factory()->create(), ['player']);
+
+        // Fetch the best player
+        $response = $this->get(route('admin.showWinner'));
+
+        // Check for a 403 status code
+        $response->assertStatus(403);
+
+        // Check JSON response structure
+        $response->assertJsonStructure([
+            'error',
+        ]);
+    }
+}
